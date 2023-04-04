6e9603a8644bc57c7a5bd552d87e83ba1ddcc585
hotteok00 <hojun7889@gmail.com>
2023-04-04 17:44:32 +0900

    first version

---
+++ ho_accountbook.js
@@ -0,0 +1,16 @@
+// Element
+const accountBook = document.getElementById('account-book');
+
+const monthlyRecord = document.getElementById('monthly-record');
+const weeklyRecord = document.getElementById('weekly-record');
+const dailyRecord = document.getElementById('daily-record');
+
+function checkElement() {
+    console.dir(accountBook);
+
+    console.dir(monthlyRecord);
+    console.dir(weeklyRecord);
+    console.dir(dailyRecord);
+}
+checkElement();
+
